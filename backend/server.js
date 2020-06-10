a735370b72da8ad4d36f690dd3f701630b5f6337
rezafarzaneh <reza@useinsider.com>
2020-06-10 16:59:53 +0300

    first init

--- backend/server.js
+++ backend/server.js
@@ -1,109 +1,20 @@
-'use strict';
+var express = require('express');
+var app = express();
+var path = require('path');
+var axios = require('axios');
 
-const Path = require('path');
-const Pkg = require(Path.join(__dirname, '..', 'package.json'));
-const express = require('express');
-//const jObj=require('jquery');
-// Helper utility for verifying and decoding the jwt sent from Salesforce Marketing Cloud.
-const verifyJwt = require(Path.join(__dirname, 'lib', 'jwt.js'));
-// Helper class that handles all the interactions with Salesforce Service Cloud
-// and makes sure open connections are reused for subsequent requests.
-var bodyParser = require('body-parser');
-const app = express();
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-// Register middleware that parses the request payload.
 
-app.use(bodyParser.raw({
-	type: 'application/jwt'
-}));
+var clientID = process.env.CLIENT_ID;
+var clientSecret = process.env.CLIENT_SECRET;
 
-// Route that is called for every contact who reaches the custom split activity
-app.post('/activity/execute', (req, res) => {
-	console.log("Body in execute->" + req.body);
-	var jwt = new verifyJwt({
-		appSignature: process.env.jwtSecret
-	});
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-	// Object representing the data in the JWT
-	var decoded = jwt.decode(req);
-	console.log("Decoded after JWT: " + JSON.stringify(decoded));
-	//console.log("decoded length"+JSON.stringify(decoded.inArguments.length));
-	if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
-		console.log("decoded->" + decoded);
-		var apikey = decoded.inArguments[0]["api_key"];
-		var title = decoded.inArguments[0]["title"];
-		var message = decoded.inArguments[0]["message"];
-		var emailKey = decoded.inArguments[0]["emailKey"];
-		var imageUrl = decoded.inArguments[0]["imageUrl"];
-		var deepLinkKey = decoded.inArguments[0]["deepLinkKey"];
-		var deepLinkVal = decoded.inArguments[0]["deepLinkVal"];
-		var channelId = decoded.inArguments[0]["channel_id"];
-		var campaignId = decoded.inArguments[0]["camp_id"];
-		var contactEmail = decoded.inArguments[0]["emailAddress"];
-		console.log("Contact Email--> " + contactEmail);
-		var jsonObj = {};
-		jsonObj["api_key"] = apikey;
-		jsonObj["notifications"] = [];
-		var notificationObj = {};
-		notificationObj["target"] = {};
-		notificationObj["target"][emailKey] = contactEmail;
-		notificationObj["title"] = title;
-		notificationObj["message"] = message;
-		notificationObj["deep_link"] = {};
-		notificationObj["deep_link"][deepLinkKey] = deepLinkVal;
-		notificationObj["image_url"] = imageUrl;
-		if (typeof channelId !== "number" || channelId < 0) {
-			channelId = 1
-		}
-		notificationObj["channel_id"] = channelId;
-		notificationObj["camp_id"] = campaignId;
-		jsonObj["notifications"].push(notificationObj);
-		console.log("Json structure: " + JSON.stringify(jsonObj));
-		var xhr = new XMLHttpRequest();
-		xhr.open("POST", "https://mobile.useinsider.com/api/v1/notification/user", true);
-		xhr.setRequestHeader("Content-Type", "application/json");
-		xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-		xhr.onreadystatechange = function (e) {
-			console.log("status -> ", xhr.status);
-			console.log("ready state ", xhr.readyState);
-			console.log("response ", xhr.responseText);
-			if (xhr.status == 200) {
-				res.status(200).end();
-			}
-		};
-		xhr.send(JSON.stringify(jsonObj));
-	} else {
-		console.error('inArguments invalid.');
-		return res.status(400).end();
-	}
+
+app.get('/', function(request, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.post('/activity/save', (req, res) => {
-	console.log("Body->" + req.body);
-	res.send(200);
-});
-
-// Routes for saving, publishing and validating the custom activity. In this case
-// nothing is done except decoding the jwt and replying with a success message.
-app.post(/\/activity\/(publish|validate)/, (req, res) => {
-	var jwt = new verifyJwt({
-		appSignature: process.env.jwtSecret
-	});
-
-	// Object representing the data in the JWT
-	var decoded = jwt.decode(req);
-	console.log("Decoded after JWT: " + JSON.stringify(decoded));
-	console.log("decode after JWT:" + decoded);
-	console.log("Calling publish");
-	console.log("Body->" + req.body);
-
-	res.send(200);
-});
-
-// Serve the custom activity's interface, config, etc.
-app.use(express.static(Path.join(__dirname, '..', 'public')));
-
-// Start the server and listen on the port specified by heroku or defaulting to 12345
-app.listen(process.env.PORT || 12345, () => {
-	console.log('Service Cloud customsplit backend is now running!');
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
 });
--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,90 +1,22 @@
 define([
-    'postmonger'
-], function (
+    'js/postmonger'
+], function(
     Postmonger
 ) {
-        'use strict';
-        var Postmonger = require('postmonger');
-        var connection = new Postmonger.Session();
-        var payload = {};
-        var eventDefinitionKey = '';
-        var deFields = [];
+    'use strict';
 
-        $(window).ready(function () {
-            connection.trigger('ready');
-            connection.trigger('requestInteraction');
-        });
+    var connection = new Postmonger.Session();
+    // create an empty payload
+    var payload = { metaData: { } };
 
-        function initialize(data) {
-            if (data) {
-                payload = data;
-            }
-            console.log("New-> " + JSON.stringify(payload));
-            var apiKey = payload["arguments"].execute.inArguments[0]["api_key"];
-            var title = payload["arguments"].execute.inArguments[0]["title"];
-            var message = payload["arguments"].execute.inArguments[0]["message"];
-            var emailKey = payload["arguments"].execute.inArguments[0]["emailKey"];
-            var imageUrl = payload["arguments"].execute.inArguments[0]["imageUrl"];
-            var deepLinkKey = payload["arguments"].execute.inArguments[0]["deepLinkKey"];
-            var deepLinkVal = payload["arguments"].execute.inArguments[0]["deepLinkVal"];
-            var channelid = payload["arguments"].execute.inArguments[0]["channel_id"];
-            var campid = payload["arguments"].execute.inArguments[0]["camp_id"];
-            $("#camp").val(campid);
-            $("#titl").val(title);
-            $("#imgurl").val(imageUrl);
-            $("#msg").val(message);
-            $("#apikey").val(apiKey);
-            $("#deep").val(deepLinkKey);
-            $("#deepval").val(deepLinkVal);
-            $("#channel").val(channelid);
-            $("#eml").val(emailKey);
-        }
-
-
-        function save() {
-		
-	
-            var campaignId = parseInt($("#camp").val());
-            var title = $("#titl").val();
-            var imageUrl = $("#imgurl").val();
-            var message = $("#msg").val();
-            var apikey = $("#apikey").val();
-            var deepLinkKey = $("#deep").val();
-            var deepLinkVal = $("#deepval").val();
-            var channelId = parseInt($("#channel").val());
-            var emailKey = $("#eml").val();
-            //Validations
-            if (channelId == "") {
-                channelId = 1;
-            }
-		payload["arguments"].execute.inArguments[0]["api_key"] = apikey;
-		payload["arguments"].execute.inArguments[0]["title"] = title;
-		payload["arguments"].execute.inArguments[0]["message"] = message;
-		payload["arguments"].execute.inArguments[0]["emailKey"] = emailKey;
-		payload["arguments"].execute.inArguments[0]["imageUrl"] = imageUrl;
-		payload["arguments"].execute.inArguments[0]["deepLinkKey"] = deepLinkKey ;
-                payload["arguments"].execute.inArguments[0]["deepLinkVal"] = deepLinkVal;
-                payload["arguments"].execute.inArguments[0]["channel_id"] = channelId;
-		payload["arguments"].execute.inArguments[0]["camp_id"] = campaignId;
-
-	/*
-            payload["arguments"].execute.inArguments = [{
-                "api_key": apikey,
-                "emailKey": emailKey,
-                "title": title,
-                "message": message,
-                "imageUrl": imageUrl,
-                "deepLinkKey": deepLinkKey,
-                "deepLinkVal": deepLinkVal,
-                "channel_id": channelId,
-                "camp_id": campaignId,
-            }];
-	    */
-	payload["metaData"]["isConfigured"] = true;
-            console.log("Payload is -> " + JSON.stringify(payload));
-            connection.trigger('updateActivity', payload);
-        }
-
-        connection.on('initActivity', initialize);
-        connection.on('clickedNext', save);
+    $(window).ready(onRender);
+    connection.on('clickedNext', onClickedNext);
+    function onRender() {
+// JB will respond the first time 'ready' is called with 'initActivity'
+connection.trigger('ready'); } function onClickedNext() {
+        /* This activity only has one step. Clicking 'next' or done should update
+        the activity payload to be marked as configured. */
+        payload.metaData.isConfigured = true;
+        connection.trigger('updateActivity', payload);
+    }
 });
