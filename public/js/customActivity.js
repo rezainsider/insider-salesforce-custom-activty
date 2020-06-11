4b1ab4a2a0bc1a87e3ef75e0b8b11943dd18aa45
rezafarzaneh <reza@useinsider.com>
2020-06-11 12:10:37 +0300

    first init

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,18 +1,12 @@
-define([
-    'postmonger'
-], function (
-    Postmonger
-) {
+define(['postmonger'], function(Postmonger) {
     'use strict';
     var Postmonger = require('postmonger');
     var connection = new Postmonger.Session();
     var payload = {};
-
-    $(window).ready(function () {
+    $(window).ready(function() {
         connection.trigger('ready');
         connection.trigger('requestInteraction');
     });
-
     function initialize(data) {
         if (data) {
             payload = data;
@@ -24,7 +18,6 @@ define([
         var image_url = payload["arguments"].execute.inArguments[0]["image_url"];
         var banner = payload["arguments"].execute.inArguments[0]["banner"];
         var push_url = payload["arguments"].execute.inArguments[0]["push_url"];
-
         $("#api_key").val(api_key);
         $("#title").val(title);
         $("#description").val(description);
@@ -32,7 +25,6 @@ define([
         $("#banner").val(banner);
         $("#push_url").val(push_url);
     }
-
     function save() {
         var api_key = $("#api_key").val();
         var title = $("#title").val();
@@ -40,19 +32,16 @@ define([
         var image_url = $("#image_url").val();
         var banner = $("#banner").val();
         var push_url = $("#push_url").val();
-
         payload["arguments"].execute.inArguments[0]["api_key"] = api_key;
         payload["arguments"].execute.inArguments[0]["title"] = title;
         payload["arguments"].execute.inArguments[0]["description"] = description;
         payload["arguments"].execute.inArguments[0]["image_url"] = image_url;
         payload["arguments"].execute.inArguments[0]["banner"] = banner;
         payload["arguments"].execute.inArguments[0]["push_url"] = push_url;
-
         payload["metaData"]["isConfigured"] = true;
         console.log("Payload is -> " + JSON.stringify(payload));
         connection.trigger('updateActivity', payload);
     }
-
     connection.on('initActivity', initialize);
     connection.on('clickedNext', save);
 });
--- public/js/postmonger.js
+++ public/js/postmonger.js
@@ -1,5 +1,4 @@
 'use strict';
-
 requirejs.config({
     paths: {
         postmonger: 'postmonger'
@@ -13,180 +12,133 @@ requirejs.config({
         }
     }
 });
-
-requirejs(['jquery-3.3.1.min', 'customActivity'], function ($, customEvent) {
-});
-
-requirejs.onError = function (err) {
+requirejs(['jquery-3.3.1.min', 'customActivity'], function($, customEvent) {});
+requirejs.onError = function(err) {
     if (err.requireType === 'timeout') {
         console.log('modules: ' + err.requireModules);
     }
     throw err;
-};
-/*
- * Postmonger.js   version 0.0.14
- * https://github.com/kevinparkerson/postmonger
- *
- * Copyright (c) 2012-2014 Kevin Parkerson
- * Available via the MIT or new BSD license.
- * Further details and documentation:
- * http://kevinparkerson.github.com/postmonger/
- *
- *///
-
-(function (root, factory) {
+}
+;
+(function(root, factory) {
     if (typeof define === 'function' && define.amd) {
-        define('postmonger', [], function () { return factory(root); });
+        define('postmonger', [], function() {
+            return factory(root);
+        });
     } else if (typeof exports === 'object') {
         module.exports = factory(root);
     } else {
-        // OR use browser globals if AMD is not present
         root.Postmonger = factory(root);
     }
-}(this, function (root) {
+}(this, function(root) {
     root = root || window;
-
     var exports = exports || undefined;
     var Postmonger;
     var previous = root.Postmonger;
     var _window = (root.addEventListener || root.attachEvent) ? root : window;
     var Connection, Events, Session;
-
-    //Set up Postmonger namespace, provide noConflict support, and version
-    if (typeof(exports) !== 'undefined') {
+    if (typeof (exports) !== 'undefined') {
         Postmonger = exports;
     } else {
         Postmonger = {};
     }
-    Postmonger.noConflict = function () {
+    Postmonger.noConflict = function() {
         root.Postmonger = previous;
         return this;
-    };
+    }
+    ;
     Postmonger.version = '0.0.14';
-
-    //Create a new Postmonger Connection
-    Connection = Postmonger.Connection = function (options) {
-        options = (typeof(options) === 'object') ? options : {};
-
+    Connection = Postmonger.Connection = function(options) {
+        options = (typeof (options) === 'object') ? options : {};
         var connect = options.connect || _window.parent;
         var from = options.from || '*';
         var to = options.to || '*';
         var self = this;
-
-        //If string, grab based on id
-        if (typeof(connect) === 'string') {
+        if (typeof (connect) === 'string') {
             connect = document.getElementById(connect);
         }
-
-        //If no connection, check for jquery object
         if (connect && !connect.postMessage && connect.jquery) {
             connect = connect.get(0);
         }
-
-        //If still no connection, check for iframe
         if (connect && !connect.postMessage && (connect.contentWindow || connect.contentDocument)) {
             connect = connect.contentWindow || connect.contentDocument;
         }
-
-        //Throw warning if connection could not be made
         if (!(connect && connect.postMessage)) {
             if (_window.console && _window.console.warn) {
                 _window.console.warn(' Warning: Postmonger could not establish connection with ', options.connect);
             }
             return false;
         }
-
         self.connect = connect;
         self.to = to;
         self.from = from;
-
         return self;
-    };
-
-    //Postmonger.Events - Hacked together from Backbone.Events and two Underscore functions.
-    Events = Postmonger.Events = function () {
+    }
+    ;
+    Events = Postmonger.Events = function() {
         var eventSplitter = /\s+/;
         var self = this;
-
         self._callbacks = {};
-
-        self._has = function (obj, key) {
+        self._has = function(obj, key) {
             return Object.prototype.hasOwnProperty.call(obj, key);
-        };
-
-        self._keys = function (obj) {
+        }
+        ;
+        self._keys = function(obj) {
             if (Object.keys) {
                 return Object.keys(obj);
             }
-
-            if (typeof(obj)!=='object') {
+            if (typeof (obj) !== 'object') {
                 throw new TypeError('Invalid object');
             }
-
             var keys = [];
-
             for (var key in obj) {
                 if (self._has(obj, key)) {
                     keys[keys.length] = key;
                 }
             }
-
             return keys;
-        };
-
-        self.on = function (events, callback, context) {
+        }
+        ;
+        self.on = function(events, callback, context) {
             var calls, event, node, tail, list;
-
             if (!callback) {
                 return self;
             }
-
             events = events.split(eventSplitter);
-
             self._callbacks = self._callbacks || {};
             calls = self._callbacks;
-
             while (event = events.shift()) {
                 list = calls[event];
-
                 node = (list) ? list.tail : {};
                 tail = {};
-
                 node.next = tail;
                 node.context = context;
                 node.callback = callback;
-
                 calls[event] = {
                     tail: tail,
                     next: (list) ? list.next : node
                 };
             }
-
             return self;
-        };
-
-        self.off = function (events, callback, context) {
+        }
+        ;
+        self.off = function(events, callback, context) {
             var calls = self._callbacks;
             var event, node, tail, cb, ctx;
-
             if (!calls) {
                 return;
             }
-
             if (!(events || callback || context)) {
                 delete self._callbacks;
                 return self;
             }
-
             events = (events) ? events.split(eventSplitter) : self._keys(calls);
-
             while (event = events.shift()) {
                 node = calls[event];
                 delete calls[event];
                 if (!node || !(callback || context)) {
                     continue;
                 }
-
                 tail = node.tail;
                 while ((node = node.next) !== tail) {
                     cb = node.callback;
@@ -196,21 +148,17 @@ requirejs.onError = function (err) {
                     }
                 }
             }
-
             return self;
-        };
-
-        self.trigger = function (events) {
+        }
+        ;
+        self.trigger = function(events) {
             var event, node, calls, tail, args, all, rest;
-
             if (!(calls = self._callbacks)) {
                 return self;
             }
-
             all = calls.all;
             events = events.split(eventSplitter);
             rest = Array.prototype.slice.call(arguments, 1);
-
             while (event = events.shift()) {
                 if (node = calls[event]) {
                     tail = node.tail;
@@ -226,27 +174,23 @@ requirejs.onError = function (err) {
                     }
                 }
             }
-
             return self;
-        };
-
+        }
+        ;
         return self;
-    };
-
-    //Create a new Postmonger Session
-    Session = Postmonger.Session = function () {
-        var args = (arguments.length>0) ? Array.prototype.slice.call(arguments, 0) : [{}];
+    }
+    ;
+    Session = Postmonger.Session = function() {
+        var args = (arguments.length > 0) ? Array.prototype.slice.call(arguments, 0) : [{}];
         var connections = [];
         var incoming = new Events();
         var outgoing = new Events();
         var self = this;
         var connection, i, j, l, ln, postMessageListener;
-
-        //Session API hooks
         self.on = incoming.on;
         self.off = incoming.off;
         self.trigger = outgoing.trigger;
-        self.end = function () {
+        self.end = function() {
             incoming.off();
             outgoing.off();
             if (_window.removeEventListener) {
@@ -255,18 +199,15 @@ requirejs.onError = function (err) {
                 _window.detachEvent('onmessage', postMessageListener);
             }
             return self;
-        };
-
-        //Establishing connections
-        for (i=0, l=args.length; i<l; i++) {
+        }
+        ;
+        for (i = 0,
+                 l = args.length; i < l; i++) {
             connection = new Connection(args[i]);
             if (connection) {
-                for (j=0, ln=connections.length; j<ln; j++) {
-                    if (
-                        connections[j].connect === connection.connect &&
-                        connections[j].from === connection.from &&
-                        connections[j].to === connection.to
-                    ) {
+                for (j = 0,
+                         ln = connections.length; j < ln; j++) {
+                    if (connections[j].connect === connection.connect && connections[j].from === connection.from && connections[j].to === connection.to) {
                         connection = null;
                         break;
                     }
@@ -276,84 +217,66 @@ requirejs.onError = function (err) {
                 }
             }
         }
-
-        //Listener for incoming messages
-        postMessageListener = function(event){
+        postMessageListener = function(event) {
             var conn = null;
             var message = [];
             var data;
             var k, len;
-
-            //Attempt to find the connection we're dealing with
-            for (k=0, len=connections.length; k<len; k++) {
+            for (k = 0,
+                     len = connections.length; k < len; k++) {
                 if (connections[k].connect === event.source) {
                     conn = connections[k];
                     break;
                 }
             }
-
-            //Check if we've found the connection
             if (!conn) {
                 return false;
             }
-
-            //Check if the message is from the expected origin
             if (conn.from !== '*' && conn.from !== event.origin) {
                 return false;
             }
-
-            //Check the data that's been passed
-            try{
+            try {
                 data = JSON.parse(event.data);
-                if(!data.e){
+                if (!data.e) {
                     return false;
                 }
-            }catch(e){
+            } catch (e) {
                 return false;
             }
-
-            //Format the passed in data
             message.push(data.e);
             delete data.e;
             for (k in data) {
                 message.push(data[k]);
             }
-
-            //Send the message
             incoming['trigger'].apply(root, message);
-        };
-
-        //Add the listener
+        }
+        ;
         if (_window.addEventListener) {
             _window.addEventListener('message', postMessageListener, false);
-        } else if(_window.attachEvent) {
+        } else if (_window.attachEvent) {
             _window.attachEvent('onmessage', postMessageListener);
-        } else{
+        } else {
             if (_window.console && _window.console.warn) {
                 _window.console.warn('WARNING: Postmonger could not listen for messages on window %o', _window);
             }
             return false;
         }
-
-        //Sending outgoing messages
-        outgoing.on('all', function () {
+        outgoing.on('all', function() {
             var args = Array.prototype.slice.call(arguments, 0);
             var message = {};
             var k, len;
-
             message.e = args[0];
-
-            for (k=1, len=args.length; k<len; k++) {
+            for (k = 1,
+                     len = args.length; k < len; k++) {
                 message['a' + k] = args[k];
             }
-
-            for (k=0, len=connections.length; k<len; k++) {
+            for (k = 0,
+                     len = connections.length; k < len; k++) {
                 connections[k].connect.postMessage(JSON.stringify(message), connections[k].to);
             }
         });
-
         return self;
-    };
-
+    }
+    ;
     return Postmonger;
 }));
