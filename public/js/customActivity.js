47540fdc125a42ee16ab6db5e29e02a3a7960780
rezafarzaneh <reza@useinsider.com>
2020-06-11 11:06:49 +0300

    first init

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,22 +1,58 @@
 define([
     'postmonger'
-], function(
+], function (
     Postmonger
 ) {
     'use strict';
-
+    var Postmonger = require('postmonger');
     var connection = new Postmonger.Session();
-    // create an empty payload
-    var payload = { metaData: { } };
+    var payload = {};
 
-    $(window).ready(onRender);
-    connection.on('clickedNext', onClickedNext);
-    function onRender() {
-// JB will respond the first time 'ready' is called with 'initActivity'
-connection.trigger('ready'); } function onClickedNext() {
-        /* This activity only has one step. Clicking 'next' or done should update
-        the activity payload to be marked as configured. */
-        payload.metaData.isConfigured = true;
+    $(window).ready(function () {
+        connection.trigger('ready');
+        connection.trigger('requestInteraction');
+    });
+
+    function initialize(data) {
+        if (data) {
+            payload = data;
+        }
+        console.log("New-> " + JSON.stringify(payload));
+        var api_key = payload["arguments"].execute.inArguments[0]["api_key"];
+        var title = payload["arguments"].execute.inArguments[0]["title"];
+        var description = payload["arguments"].execute.inArguments[0]["description"];
+        var image_url = payload["arguments"].execute.inArguments[0]["image_url"];
+        var banner = payload["arguments"].execute.inArguments[0]["banner"];
+        var push_url = payload["arguments"].execute.inArguments[0]["push_url"];
+
+        $("#api_key").val(api_key);
+        $("#title").val(title);
+        $("#description").val(description);
+        $("#image_url").val(image_url);
+        $("#banner").val(banner);
+        $("#push_url").val(push_url);
+    }
+
+    function save() {
+        var api_key = $("#api_key").val();
+        var title = $("#title").val();
+        var description = $("#description").val();
+        var image_url = $("#image_url").val();
+        var banner = $("#banner").val();
+        var push_url = $("#push_url").val();
+
+        payload["arguments"].execute.inArguments[0]["api_key"] = api_key;
+        payload["arguments"].execute.inArguments[0]["title"] = title;
+        payload["arguments"].execute.inArguments[0]["description"] = description;
+        payload["arguments"].execute.inArguments[0]["image_url"] = image_url;
+        payload["arguments"].execute.inArguments[0]["banner"] = banner;
+        payload["arguments"].execute.inArguments[0]["push_url"] = push_url;
+
+        payload["metaData"]["isConfigured"] = true;
+        console.log("Payload is -> " + JSON.stringify(payload));
         connection.trigger('updateActivity', payload);
     }
+
+    connection.on('initActivity', initialize);
+    connection.on('clickedNext', save);
 });
